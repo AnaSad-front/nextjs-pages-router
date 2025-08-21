2f6b54517c08f60331cdff70134e338379d7b2f2
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-08-21 15:30:54 +0300

    Fetching data with getStaticProps (SSG)

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://ozypwcjazsftxptuudhg.supabase.co",
+        hostname: "ozypwcjazsftxptuudhg.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
